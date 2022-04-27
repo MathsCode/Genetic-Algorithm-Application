c066bc26119f6f8cbcce156d558b5615f0431783
MathsCode <nthaxjm@126.com>
2022-04-27 18:51:34 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+Description: main file
+Author: Xu Jiaming
+Date: 2022-04-27 17:47:59
+LastEditTime: 2022-04-27 18:41:35
+LastEditors:  
+FilePath: main.py
+'''
+
+
+def init():
+    # 种群大小g
+    # F失效节点列表
+    # m代表有多少个失效节点
+    # n代表总共多少个节点1-n
+    g = eval(input("population size:"))  
+    n = eval(input("total nodes                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     "))  
+    m = eval(input("failure nodes number:"))
+    F = []
+    while(m):
+        F.append(eval(input()))
+        m -= 1
+        
